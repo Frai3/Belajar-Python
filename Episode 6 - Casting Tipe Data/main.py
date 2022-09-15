854b19a09e2a53a8028a837a39704787ea8df937
Frai3 <farhan.raihan@student.umn.ac.id>
2022-09-15 17:05:23 +0700

    Belajar Python

---
+++ Episode 6 - Casting Tipe Data/main.py
@@ -0,0 +1,18 @@
+#Merubah tipe data dengan Casting
+
+data_int = 10
+print("Nilai: ", data_int)
+print("Tipe: ", type(data_int))
+
+data_float = float(data_int)
+print("Nilai: ", data_float)
+print("Tipe: ", type(data_float))
+
+data_str = str(data_int)
+print("Nilai: ", data_str)
+print("Tipe: ", type(data_str))
+
+#Bool jika nilai 0 maka False, selain itu akan True
+data_bool = bool(data_int)
+print("Nilai: ", data_bool)
+print("Tipe: ", type(data_bool))
